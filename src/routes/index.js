a6aa59e6c8cc234f486f9548227493f8f6895223
jenniekibiri <josphatje@gmail.com>
2020-04-12 23:32:24 +0300

    create a heroku start script

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const estimator = require('../estimator.js');
 
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
