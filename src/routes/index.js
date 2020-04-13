b415f99183fa99770268d34b41e52961ab913cb9
jenniekibiri <josphatje@gmail.com>
2020-04-13 09:19:07 +0300

    test APi

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,16 +8,19 @@ const estimator = require('../estimator.js');
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('hello sever');
 });
 app.post('/api/v1/on-covid-19', (req, res) => {
   const {
     // eslint-disable-next-line max-len
-    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation, periodType, timeToElapse, reportedCases, population, totalHospitalBeds
+    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
   } = req.body;
-
+  const {
+    periodType, timeToElapse, reportedCases, population, totalHospitalBeds
+  } = req.body;
   const inputData = {
     region: {
       name,
@@ -34,6 +37,7 @@ app.post('/api/v1/on-covid-19', (req, res) => {
 
 
   const data = estimator(inputData);
+  // const output = covid19ImpactEstimator();
 
   res.status(200).json(data);
 });
