9550030ac4015973eebf7ef81b60cf5493448183
jenniekibiri <josphatje@gmail.com>
2020-04-13 11:11:55 +0300

    add xml endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const covid19ImpactEstimator = require('../estimator.js');
 const estimator = require('../estimator.js');
+const xmlParser = require('./fast-xml-parser');
 
 
 const app = express();
@@ -38,9 +39,44 @@ app.post('/api/v1/on-covid-19', (req, res) => {
 
   const data = estimator(inputData);
   // const output = covid19ImpactEstimator();
-
+  res.type('application/json');
+  res.set('Content-Type', 'application/json');
   res.status(200).json(data);
 });
+app.post('/api/v1/on-covid-19/xml', (req, res) => {
+  const {
+    // eslint-disable-next-line max-len
+    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
+  } = req.body.region;
+  const {
+    periodType, timeToElapse, reportedCases, population, totalHospitalBeds
+  } = req.body;
+  const inputData = {
+    region: {
+      name,
+      avgAge,
+      avgDailyIncomeInUSD,
+      avgDailyIncomePopulation
+    },
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  };
+
+
+  const data = estimator(inputData);
+  // const output = covid19ImpactEstimator();
+  // res.type('application/json');
+  // res.set('Content-Type', 'application/json');
+  // res.status(200).json(data);
+  const xmlOutput = xmlParser.parse({ root: data });
+  res.set('Content-Type', 'application/xml');
+  res.status(200);
+  res.send(xmlOutput);
+});
+
 app.post('/json', (req, res) => {
   const inputData = {
     region: {
